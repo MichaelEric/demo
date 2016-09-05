9fe1bc6c1ca8cebe75764c085fa44e52aad4a7f2
MichaelEric <1311129496@qq.com>
2016-09-05 16:09:34 +0800

    unit

---
+++ study/javascript/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jiangze on 2016/8/29.
+ */
+function showHide()
+{
+    var oDiv=document.getElementById('div1');
+
+    if(oDiv.style.display=='block')
+    {
+        oDiv.style.display='none';
+    }
+    else
+    {
+        oDiv.style.diaplay='block';
+    }
+}
